a1f50948d08969e7b381e13c302c3800e37d643c
Nicolas <nicolas260804@gmail.com>
2025-01-01 17:43:08 -0500

    con props

--- src/componentes/Perro.jsx
+++ src/componentes/Perro.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
 import '../hoja-estilo/perro.css'
+//Ejemplo 1 video: Cómo Crear una Página Web en React: Componentes, Funciones, Estilos e Importaciones
+// Componente con su respectiva funcion
+
+/*function Perro() {
+  return (
+    <div className='contenedor-perro'>
+<img className='imagen-perro' src={require('../imagenes/animal-perro.jpg')}  />
+<div className='contenedor-texto-perro'>
+  <p className='nombre-perro'>Firualis</p>
+  <p className='purina-perro'>Dog Chow.</p>
+  <p className='texto-perro'>La purina es un compuesto nitrogenado que desempeña un papel crucial en la producción de ADN, ARN y ATP (trifosfato de adenosina) en el organismo . Está presente de forma natural en diversos alimentos, entre ellos, carnes, mariscos, legumbres y ciertas verduras.</p>
+</div>
+    </div>
+  )
+}*/
+
+
+
+//video de Ejercicio con props
+// funcion con props
 function Perro(props) {
   return (
     <div className='contenedor-perro'>
