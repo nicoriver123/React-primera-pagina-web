408808df5b4488718255d1a9698d0737007ce077
Nicolas <nicolas260804@gmail.com>
2025-01-01 17:30:03 -0500

    ejemplo con props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,30 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>El mejor Perro del mundo</h1>
-      <Perro/>
+      <Perro
+      imagen='perro'
+      nombre='Firulais'
+      purina='Dog Chaow'
+      texto='Mamífero doméstico de la familia de los cánidos, de tamaño, forma y pelaje muy diversos, según las razas, que tiene olfato muy fino y es inteligente y muy leal a su dueño.'
+      
+      />
+
+<Perro
+imagen='cachorro'
+      nombre='Zeus'
+      purina='cant dos'
+      texto='“No importa lo que hagas o como le trates, el amor de los perros es infinito, su cariño es inmenso y su fidelidad es incondicional”.
+“No hay compañero tan fiel como un perro, ni amigo más leal. ...
+“No importa lo malo que haya sido tu día.'
+      
+      />
+       <Perro
+       imagen='firulais'
+      nombre='Paco'
+      purina='Dog Chaow 43'
+      texto='El perro es un mamífero carnívoro de la familia de los cánidos, que constituye una subespecie del lobo. Su tamaño o talla, su forma y su pelaje son diversos según la raza. Posee un oído y un olfato muy desarrollados. Este último es su principal órgano sensorial.'
+      
+      />
     </div>
     </div>
   );
--- src/componentes/Perro.jsx
+++ src/componentes/Perro.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../hoja-estilo/perro.css'
-function Perro() {
+function Perro(props) {
   return (
     <div className='contenedor-perro'>
-<img className='imagen-perro' src={require('../imagenes/animal-perro.jpg')}  />
+<img className='imagen-perro' src={require(`../imagenes/animal-${props.imagen}.jpg`)}  alt='foto_perro' />
 
 <div className='contenedor-texto-perro'>
-  <p className='nombre-perro'>Firualis</p>
-  <p className='purina-perro'>Dog Chow.</p>
-  <p className='texto-perro'>La purina es un compuesto nitrogenado que desempeña un papel crucial en la producción de ADN, ARN y ATP (trifosfato de adenosina) en el organismo . Está presente de forma natural en diversos alimentos, entre ellos, carnes, mariscos, legumbres y ciertas verduras.</p>
+  <p className='nombre-perro'>{props.nombre}</p>
+  <p className='purina-perro'>{props.purina}</p>
+  <p className='texto-perro'>{props.texto}</p>
 </div>
 
 
